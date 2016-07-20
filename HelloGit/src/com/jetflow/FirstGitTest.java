11a111e4978355a1f895a29800051c8d8a619ff4
徐平 <xu.ping@zte.com.cn>
2016-07-20 15:05:58 +0800

    ceshi

--- HelloGit/src/com/jetflow/FirstGitTest.java
+++ HelloGit/src/com/jetflow/FirstGitTest.java
@@ -3,7 +3,7 @@ package com.jetflow;
 public class FirstGitTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git!!!");
+		System.out.println("Hello Git!!!---------------");
 
 	}
 
